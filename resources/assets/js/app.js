66d258f12e41d601dacf45f53ca9ca7c0cb752c1
Craig Morris <cr@igmorr.is>
2018-04-10 12:02:09 +0100

    first demo

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,6 +10,12 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 /**
+ * Init Firebase.
+ */
+var VueFire = require('vuefire')
+Vue.use(VueFire)
+
+/**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,23 +1,40 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>When</th>
+                <th>Where</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="booking in bookings" :key="booking.name">
+                <th>{{ booking.name }}</th>
+                <td>{{ booking.when }}</td>
+                <td>{{ booking.where }}</td>
+                <td>{{ booking.status }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
+    var firebase = require('firebase')
+
+    var firebaseApp = firebase.initializeApp({
+        apiKey: "AIzaSyAWvZ8YXAsdLqi5InT_yj7gyfttQ5Np4XU",
+        authDomain: "laravel-meetup-demo.firebaseapp.com",
+        databaseURL: "https://laravel-meetup-demo.firebaseio.com",
+        projectId: "laravel-meetup-demo",
+        storageBucket: "laravel-meetup-demo.appspot.com",
+        messagingSenderId: "712900540679"
+    })
+    var db = firebaseApp.database()
+
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        firebase: {
+            bookings: db.ref('bookings').orderByChild('status').equalTo('paid'),
         }
     }
 </script>
